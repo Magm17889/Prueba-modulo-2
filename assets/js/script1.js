87199faa9c45272277018783117857aaf47fcc48
Miguel Gonzalez Moll <magonzalezm17@outlook.com>
2024-08-23 22:02:10 -0400

    se agrega manipulacion del dom usando la funcion evet

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -1,61 +1,30 @@
-// Arrays o arreglos
-
-let miArreglo = ["Pedro","Juan","Diego"];
-
-console.log("Mi arreglo tiene "+ miArreglo.length +" elementos")
-
-console.log("El segundo elemento de mi arreglo es: "+ miArreglo[1])
-
-miArreglo.push("Ignacio")
-
-console.log(miArreglo)
-
-miArreglo.pop()
-
-console.log(miArreglo)
 
 
-//Objetos
-let miAuto = {
-    color:"rojo",
-    marca:"nissan",
-    modelo:"versa"
-}
+//// pruebas de primeras funciones con botones pero no me funciono //// 
+/* function myFunction() {
+    alert("Que disfrutes la musica");
+  }
+const nombre = document.querySelector(".nombre");
+function cambiacolor() { 
+    nombre.style.color = "blue"; }
 
-console.log(miAuto.marca)
+function changeColor(newColor) {
+        var elem = document.getElementById("cambio_de_color");
+        elem.style.color = newColor;
+      }
+*/
+      // Cuando el mouse pasa por encima de algún elemento
+window.addEventListener('mouseover', function(evt) {
+  // Si elemento tiene la clase `Hola`
+  if (evt.target.classList.contains('Hola')) {
+    evt.target.style.color = 'blue';
+  }
+});
 
-miAuto.año = 2020
-
-console.log(miAuto)
-
-
-let arregloNumeros = []
-
-//condición inicial; condición final; incremento
-for(let i = 0; i < 10; i++){
-    console.log(i);
-    arregloNumeros.push(i);
-}
-
-console.log(arregloNumeros)
-
-//iterador en iterable
-for(posicion in miArreglo){
-    console.log(posicion);
-    console.log(miArreglo[posicion]);
-}
-
-for(atributo in miAuto){
-    console.log(atributo)
-    console.log(miAuto[atributo])
-}
-
-for (element in miAuto) {
-    console.log(`${element}: ${miAuto[element]}`);
-    console.log(element+" : "+miAuto[element]);
-}
-
-function miFuncion(num1,num2){
-    return num1+num2
-}
-
+// Cuando el mouse salga de encima de algún elemento
+window.addEventListener('mouseout', function(evt) {
+  // Si elemento tiene la clase `Hola`
+  if (evt.target.classList.contains('Hola')) {
+    evt.target.style.color = 'black';
+  }
+});

29249f0a67502f434481ff5107c9c003d1cec97f
Miguel Gonzalez Moll <magonzalezm17@outlook.com>
2024-08-24 20:44:39 -0400

    Se cargan nuevas funciones de js a index_3 y se agrega comentarios a style

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -1,10 +1,10 @@
-
-
-//// pruebas de primeras funciones con botones pero no me funciono //// 
-/* function myFunction() {
+/// Saludo de Disfruta la musica /// 
+function myFunction() {
     alert("Que disfrutes la musica");
   }
-const nombre = document.querySelector(".nombre");
+
+  //// pruebas de primeras funciones con botones pero no me funciono //// 
+/* const nombre = document.querySelector(".nombre");
 function cambiacolor() { 
     nombre.style.color = "blue"; }
 
@@ -28,3 +28,13 @@ window.addEventListener('mouseout', function(evt) {
     evt.target.style.color = 'black';
   }
 });
+
+// esta funcion me funciona bien el cambio de fondo azul y letras blancas ////
+function cambiarFondo(){
+  document.getElementById("contenido").style.backgroundColor = "blue"; document.getElementById("contenido").style.color = "white";
+}
+
+//// esta funcion funciona solo la 1 parte la segunda no, trate de hacerlo igual a la de arriba cambiandole el style color a black pero sin exito // 
+function normalizar(){
+  document.getElementById("contenido").style.backgroundColor = "white"; document.getElementById("contenido").style.Color ="black"; 
+}
